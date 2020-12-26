6d2538f56c2aba64ede489858017c5a83ed8dfe2
manmordev <manmordev@protonmail.com>
2020-12-25 17:25:18 -0800

    Fixes

--- Chapter 2/Piece.java
+++ Chapter 2/Piece.java
@@ -1,3 +1,23 @@
 public class Piece {
+    private String name;
+    private String color;
+
+    public Piece(){
+    }
     
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        Piece p = new Piece();
+    }
 }
